afd4275de085c35460acbc6dc6f796b8161e51d7
m-ariza <m.ariza214@gmail.com>
2017-04-01 21:50:21 -0400

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,100 +1,49 @@
 var $root = $('html, body');
 var $wHeight = $(window).height();
 var $wWidth = $(window).width();
-var $navBar = $('.nav > a');
+var $links = $('.link');
 var $menuBtn = $('.menu-icon');
 
-$(window).scroll(function(){
-    var $wScroll = $(this).scrollTop();
-    console.log($wScroll);
 
-
-    $('.logo').css({
-        'transform':'translate(0px, -'+ $wScroll /3 +'%)'
-    });
-});
 $menuBtn.click(function() {
-    $navBar.each(function(){
-        $(this).slideToggle(500);
-    });
+
+
+    if($wWidth >500 && $wWidth <820){
+        $('.links-container').animate({
+            'width': 'toggle'
+        });
+
+    }else {
+    }
 });
 
 
-$navBar.click(function () {
-    var href = $.attr(this, 'href');
-    $('body').animate({
-        opacity: '0'
 
-    }, 200, function(){
-        $('body').animate({
-            opacity: '1'
-        });
-    });
+$links.click(function () {
+
+    var href = $.attr(this, 'href');
 
     $root.animate({
         scrollTop: $(href).offset().top
-    },200, function () {
+    },500, function () {
         window.location.hash = href;
     });
-    return false;
+    $('.links-container').slideToggle(300);
 });
+
+/*
 $(window).resize(function(){
     var $wWidth = $(window).width();
 
     if ($wWidth < 650) {
        $navBar.addClass('mobile-nav');
        $('.logo').hide(500);
-
-        console.log('hello');
+       console.log('hello');
        $menuBtn.slideDown(100);
-       $navBar.click(function () {
-            var href = $.attr(this, 'href');
-        });
     }
     else {
         $('.logo').show(200);
-        $navBar.removeClass('mobile-nav');
         $menuBtn.slideUp(0);
-
-
     }
 });
-$(window).scroll(function () { /*pushes logo to background; makes logo BIGGER when
- screen scrolls halfway down*/
-
-    var $wScroll = $(window).scrollTop();
-
-
-    if ($wScroll < ($wHeight/10)) {
-
-        $('.logo').stop(true, false).animate({
-            'opacity': '.7'
-        },300);
-        $navBar.css({
-            'color': 'rgb(255, 255, 255, 1)',
-        });
-        $('.nav').animate({
-            'top': '45%'
-        });
-
-        console.log('logo appear');
-    }
-
-    else {if ($wScroll > ($wHeight/10) ) {
-        $('.logo').stop(true, false).animate({
-            'opacity':'0'
-        },300);
-        $navBar.css({
-            'color': 'white'
-        });
-        $('.nav').animate({
-            'top': '0'
-        });
-        console.log('logo disappear');
-    }}
-
-
-    // Do something
-});
-
-
+*/
