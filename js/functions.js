c0faa198482ca0141228507e02fba1c2f328f064
m-ariza <m.ariza214@gmail.com>
2017-04-11 16:28:04 -0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -3,18 +3,31 @@ var $wHeight = $(window).height();
 var $wWidth = $(window).width();
 var $links = $('.link');
 var $menuBtn = $('.menu-icon');
+var $wScroll=$(window).scrollTop();
+var $leftPageTop = $('.left-title-page').offset().top;
+var $rightPageTop =  $('.right-content').offset().top;
+
+console.log($rightPageTop);
 
 $(window).scroll(function(){
-    var $wScroll=$(this).scrollTop();
-    
-    if($wScroll > $('.left-title-page').offset().top* .6){
+    var $wScroll=$(window).scrollTop();
 
+
+
+
+    if($wScroll > $leftPageTop *.3){
         $('.page-name').addClass('is-showing');
-        console.log('hello there!')
+
     }else{
         $('.page-name').removeClass('is-showing');
 
-        console.log('ho there')
+    }
+
+
+    if($wScroll > $rightPageTop*.5){
+        $('.about-me').fadeIn(900);
+    }else{
+        $('.about-me').fadeOut(500);
     }
 
 
@@ -43,10 +56,10 @@ $links.click(function() {
     }
 
     var href = $.attr(this, 'href');
-
     $root.animate({
         scrollTop: $(href).offset().top
-    },500, function () {
+    },0, function () {
         window.location.hash = href;
     });
+
 });
